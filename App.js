843b32d6b62761cae455f9c84d814e884effe1bc
kawabata324 <kawabataharuki48@gmail.com>
2023-05-17 08:18:38 +0900

    :sparkles: news 記事一覧表示

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
+import { useState, useEffect } from "react"
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { SafeAreaView, StyleSheet, FlatList } from "react-native"
+import { ListItem } from "./src/components/ListItem"
+import axios from "axios"
+import Constants from "expo-constants"
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
 
 export default function App() {
+  const [articles, setArticles] = useState([])
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL)
+      setArticles(response.data.articles)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#eee",
   },
 })
