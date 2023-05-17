eaa2cb742e09621a0af8262350cc722a0c81d6f6
kawabata324 <kawabataharuki48@gmail.com>
2023-05-17 09:00:27 +0900

    :sparkles: 詳細の作成

--- App.js
+++ App.js
@@ -1,43 +1,23 @@
-import { useState, useEffect } from "react"
-import { StatusBar } from "expo-status-bar"
-import { SafeAreaView, StyleSheet, FlatList } from "react-native"
-import { ListItem } from "./src/components/ListItem"
-import axios from "axios"
-import Constants from "expo-constants"
+import { NavigationContainer } from "@react-navigation/native"
+import { HomeScreen } from "./screens/HomeScreen"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { ArticleScreen } from "./screens/ArticleScreen"
 
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  const [articles, setArticles] = useState([])
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL)
-      setArticles(response.data.articles)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  useEffect(() => {
-    fetchArticles()
-  }, [])
-
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />}
-        keyExtractor={(item, index) => index.toString()}
-      />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="Article" component={ArticleScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#eee",
-  },
-})
